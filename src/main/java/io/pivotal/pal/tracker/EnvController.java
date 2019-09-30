32b7285df4bd74c30688e7029b54bffaca17cda8
pal_user <Venky>
2019-09-30 15:08:29 -0600

    End of Day 1 Commit

--- src/main/java/io/pivotal/pal/tracker/EnvController.java
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -18,7 +18,7 @@ public class EnvController {
 
     private String cfInstanceAddr;
 
-    public EnvController(@Value("${port:NOT SET}") String port, @Value("${memory.limit:NOT SET}") String memoryLimit, @Value("${cf.instance.index:NOT SET}") String cfInstanceIndex, @Value("{cf.instance.addr:NOT SET}") String cfInstanceAddr)
+    public EnvController(@Value("${port:NOT SET}") String port, @Value("${memory.limit:NOT SET}") String memoryLimit, @Value("${cf.instance.index:NOT SET}") String cfInstanceIndex, @Value("${cf.instance.addr:NOT SET}") String cfInstanceAddr)
     {
         this.port=port;
         this.memoryLimit=memoryLimit;
